487fe2570dbaca32714c3748f0067e362f783b11
jbmc99 <127203600+jbmc99@users.noreply.github.com>
2024-07-19 14:10:18 +0200

    upp

---
+++ src/components/Movies.jsx
@@ -0,0 +1,26 @@
+export function ListOfMovies ({ movies }) {
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li key={movie.imdbID}>
+          <h3>{movie.Title}</h3>
+          <p>{movie.Year}</p>
+          <img src={movie.Poster} alt={movie.Title} />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export function NoMoviesResults () {
+  return (
+    <div>
+      <h2>No se encontraron resultados</h2>
+      <p>Intenta con otra búsqueda</p>
+    </div>
+  )
+}
+
+export function Movies () {
+    
+}
