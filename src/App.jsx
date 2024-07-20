9caacab2a2504a82d6ac643447fe69433f32f820
jbmc99 <127203600+jbmc99@users.noreply.github.com>
2024-07-20 14:18:16 +0200

    up!

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,48 @@
 import './App.css'
-import responseMovies from './mocks/with-results.json'
-import withoutResults from './mocks/no-results.json'
+// useRef es un hook que permite crear una referencia mutable que permite durante
+// todo el ciclo de vida del componente, util para guardar cualqier valor que puedas mutar
+// como un valor, un contador, un elemento del dom... etc
+import { useMovies } from './hooks/useMovies'
 import { Movies } from './components/Movies'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
-function App ( ) {
-  const movies = responseMovies.Search
+function App () {
+  const { movies } = useMovies()
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log({ query })
+  }
+
+  const handleChange = (event) => {
+    setQuery(event.target.value)
+  }
+
+  useEffect(() => {
+    if (query === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+    if (query.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+    if (query.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+    setError(null)
+  }, [query])
 
   return (
     <div className='page'>
       <header>
         <h1>Buscador de películas</h1>
-        <form className='form'>
-          <input placeholder='Avengers, Star Wars, The Matrix...' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input onChange={handleChange} value={query} placeholder='Avengers, Star Wars, The Matrix...' />
           <button type='submit'>Buscar</button>
         </form>
       </header>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,10 +3,10 @@ export function ListOfMovies ({ movies }) {
   return (
     <ul>
       {movies.map((movie) => (
-        <li key={movie.imdbID}>
-          <h3>{movie.Title}</h3>
-          <p>{movie.Year}</p>
-          <img src={movie.Poster} alt={movie.Title} />
+        <li key={movie.id}>
+          <h3>{movie.title}</h3>
+          <p>{movie.year}</p>
+          <img src={movie.poster} alt={movie.Title} />
         </li>
       ))}
     </ul>
@@ -23,6 +23,6 @@ export function NoMoviesResults () {
 }
 
 export function Movies ({ movies }) {
-  const hasMovies = movies?.length > 0;
+  const hasMovies = movies?.length > 0
   return hasMovies ? <ListOfMovies movies={movies} /> : <NoMoviesResults />;
 }
