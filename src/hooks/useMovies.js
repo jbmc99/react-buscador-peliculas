de0e088b44426687cee60d7fe7c5bb89b8ddd0b1
jbmc99 <127203600+jbmc99@users.noreply.github.com>
2024-07-21 19:25:37 +0200

    fetch api

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,12 +1,30 @@
-import responseMovies from '../mocks/with-results.json'
+import withResults from '../mocks/with-results.json'
 import withoutResults from '../mocks/no-results.json'
-export function useMovies () {
+import { useState } from 'react'
+export function useMovies ({ search }) {
+  const [responseMovies, setResponseMovies] = useState({})
+
   const movies = responseMovies.Search
+
   const mappedMovies = movies?.map((movie) => ({
     id: movie.imdbID,
     title: movie.Title,
     year: movie.Year,
     poster: movie.Poster
   }))
-  return { movies: mappedMovies }
+
+  const getMovies = () => {
+    if (search) {
+      // setResponseMovies(withResults)
+      fetch(`http://www.omdbapi.com/?apikey=4287ad07&s=${search}`)
+        .then((res) => res.json())
+        .then((json) => {
+          setResponseMovies(json)
+        })
+    } else {
+      setResponseMovies(withoutResults)
+    }
+  }
+
+  return { movies: mappedMovies, getMovies }
 }
